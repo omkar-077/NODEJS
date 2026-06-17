c1756efd38d36ab541ef6b5545a785274dd3201d
omkar-077 <wableomkar349@gmail.com>
2026-06-17 12:37:06 +0530

    New Changes Done

---
+++ Spotify/src/models/user.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        unique: true,
+        require: true
+    },
+    email:{
+        type: String,
+        unique: true,
+        require: true
+    },
+    password:{
+        type: String,
+        require: true
+    },
+    role:{
+        type: String,
+        enum :["user", "artist"],   // enum => defines types of users
+        default: "user"
+    }
+})
